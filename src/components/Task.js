84ae0398bb002f43a2437cf4543b85667ad2abbb
resant18 <santoso.renata@gmail.com>
2020-05-02 13:45:17 -0700

    Add selected task styling

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Callout, Card, Button } from "@blueprintjs/core";
 
 const SELECTED_COLOR = "#106ba3";
 const Task = ({ name }) => {
   const taskTestId = `task-${name.split(" ").join("-")}`;
+  const [isSelected, setIsSelected] = useState(false);
     
   return (
-    <div data-testid={taskTestId}>{name}</div>
+     <div
+        data-testid={taskTestId}
+        style={{ backgroundColor: isSelected ? SELECTED_COLOR : "#ffffff" }}
+        onClick={(e) => setIsSelected(!isSelected)}
+     >
+        {name}
+     </div>
   );
 };
 
